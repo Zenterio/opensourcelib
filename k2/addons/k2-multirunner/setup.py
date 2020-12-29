2d84742f19e0378ecfe5b26a7c28c85bc7c8a129
Per Böhlin <per.bohlin@devconsoft.se>
2020-12-29 11:27:03 +0100

    Added k2 multirunner addon. Extracted from a multi-purpose addon and has not been run in isolation.

---
+++ k2/addons/k2-multirunner/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name='k2-multirunner',
+    version='0.0.1+' + os.getenv('BUILD_NUMBER', '0'),
+    description='K2 Multirunner',
+    long_description=(
+        'Alternative runner to the built-in runner. Allows multiple sub-runners to collectively generate a vertict.'),
+    maintainer='Zenterio AB',
+    maintainer_email='foss@zenterio.com',
+    license='Apache 2.0',
+    packages=find_packages(
+        exclude=[
+            '*.test', '*.test.*', 'test.*', 'test', '*.systest', '*.systest.*', 'systest.*',
+            'systest'
+        ]),
+    install_requires=[''],
+    entry_points={
+        'k2.addons': [
+            'multirunner = multirunner.multirunner:MultiRunner',
+        ],
+    },
+)
